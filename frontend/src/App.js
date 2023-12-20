62608cbf1a26d9b2b691c38038bc0450a58d6dad
Minilik94 <minilikzeru@gmail.com>
2023-12-20 19:28:00 +0300

    Frontend Authentication | Redux Toolkit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import  'react-toastify/dist/ReactToastify.css'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Header from './components/Header'
+import Spinner from './components/Spinner'
+
 
 function App() {
+
+
   return (
     <>
       <Router>
@@ -15,6 +21,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Routes>
+          <ToastContainer/>
         </div>
       </Router>
     </>
